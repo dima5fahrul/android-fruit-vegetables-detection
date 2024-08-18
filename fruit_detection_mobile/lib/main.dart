57df9526274aa270ee4d78c74d6dd7442960c527
Dimas Fahrul <dfpa.05@gmail.com>
2024-08-18 21:40:03 +0700

    add: new version

--- fruit_detection_mobile/lib/main.dart
+++ fruit_detection_mobile/lib/main.dart
@@ -1,10 +1,17 @@
 import 'dart:io';
 
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+late List<CameraDescription> _cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  _cameras = await availableCameras();
   runApp(const MyApp());
 }
 
@@ -20,69 +27,156 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomePageScreen(),
+      home: const _LiveDetection(),
     );
   }
 }
 
-class HomePageScreen extends StatefulWidget {
-  const HomePageScreen({super.key});
+class _LiveDetection extends StatefulWidget {
+  const _LiveDetection({super.key});
 
   @override
-  State<HomePageScreen> createState() => _HomePageScreenState();
+  State<_LiveDetection> createState() => _LiveDetectionState();
 }
 
-class _HomePageScreenState extends State<HomePageScreen> {
-  File? image;
-  late ImagePicker imagePicker;
-  late ImageLabeler labeler;
-  String results = "";
+class _LiveDetectionState extends State<_LiveDetection> {
+  late CameraController controller;
+  bool isBusy = false;
+  String result = "";
+  late ImageLabeler imageLabeler;
 
   @override
   void initState() {
     super.initState();
-    imagePicker = ImagePicker();
-    final ImageLabelerOptions options =
-        ImageLabelerOptions(confidenceThreshold: 0.5);
-    labeler = ImageLabeler(options: options);
+    // imageLabeler =
+    //     ImageLabeler(options: ImageLabelerOptions(confidenceThreshold: 0.5));
+    loadModel();
+    controller = CameraController(
+      _cameras[0],
+      ResolutionPreset.max,
+      imageFormatGroup: Platform.isAndroid
+          ? ImageFormatGroup.nv21 // for Android
+          : ImageFormatGroup.bgra8888,
+    );
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      controller.startImageStream((image) => {
+            if (!isBusy) {isBusy = true, _imageLabeling(image)}
+          });
+
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            // Handle access errors here.
+            break;
+          default:
+            // Handle other errors here.
+            break;
+        }
+      }
+    });
   }
 
-  void chooseImage() async {
-    XFile? selectedImage =
-        await imagePicker.pickImage(source: ImageSource.gallery);
-    if (selectedImage != null) {
-      setState(() => image = File(selectedImage.path));
-      performImageLabeling();
+  _imageLabeling(CameraImage img) async {
+    result = "";
+    InputImage? inputImg = _inputImageFromCameraImage(img);
+    debugPrint('img: $img');
+    debugPrint("Image: $inputImg");
+    if (inputImg != null) {
+      final List<ImageLabel> labels = await imageLabeler.processImage(inputImg);
+      for (ImageLabel label in labels) {
+        final String text = label.label;
+        final int index = label.index;
+        final double confidence = label.confidence;
+        result += "$text   ${confidence.toStringAsFixed(2)}\n";
+      }
+      setState(() {
+        result;
+        isBusy = false;
+      });
     }
   }
 
-  void captureImage() async {
-    XFile? capturedImage =
-        await imagePicker.pickImage(source: ImageSource.camera);
-    if (capturedImage != null) {
-      setState(() => image = File(capturedImage.path));
-      performImageLabeling();
+  final _orientations = {
+    DeviceOrientation.portraitUp: 0,
+    DeviceOrientation.landscapeLeft: 90,
+    DeviceOrientation.portraitDown: 180,
+    DeviceOrientation.landscapeRight: 270,
+  };
+
+  InputImage? _inputImageFromCameraImage(CameraImage image) {
+    // get image rotation
+    // it is used in android to convert the InputImage from Dart to Java
+    // `rotation` is not used in iOS to convert the InputImage from Dart to Obj-C
+    // in both platforms `rotation` and `camera.lensDirection` can be used to compensate `x` and `y` coordinates on a canvas
+    final camera = _cameras[1];
+    final sensorOrientation = camera.sensorOrientation;
+    InputImageRotation? rotation;
+    if (Platform.isIOS) {
+      rotation = InputImageRotationValue.fromRawValue(sensorOrientation);
+    } else if (Platform.isAndroid) {
+      var rotationCompensation =
+          _orientations[controller!.value.deviceOrientation];
+      if (rotationCompensation == null) return null;
+      if (camera.lensDirection == CameraLensDirection.front) {
+        // front-facing
+        rotationCompensation = (sensorOrientation + rotationCompensation) % 360;
+      } else {
+        // back-facing
+        rotationCompensation =
+            (sensorOrientation - rotationCompensation + 360) % 360;
+      }
+      rotation = InputImageRotationValue.fromRawValue(rotationCompensation);
     }
+    if (rotation == null) return null;
+
+    // get image format
+    final format = InputImageFormatValue.fromRawValue(image.format.raw);
+    // validate format depending on platform
+    // only supported formats:
+    // * nv21 for Android
+    // * bgra8888 for iOS
+    if (format == null ||
+        (Platform.isAndroid && format != InputImageFormat.nv21) ||
+        (Platform.isIOS && format != InputImageFormat.bgra8888)) return null;
+
+    // since format is constraint to nv21 or bgra8888, both only have one plane
+    if (image.planes.length != 1) return null;
+    final plane = image.planes.first;
+
+    // compose InputImage using bytes
+    return InputImage.fromBytes(
+      bytes: plane.bytes,
+      metadata: InputImageMetadata(
+        size: Size(image.width.toDouble(), image.height.toDouble()),
+        rotation: rotation, // used only in Android
+        format: format, // used only in iOS
+        bytesPerRow: plane.bytesPerRow, // used only in iOS
+      ),
+    );
   }
 
-  Future<void> performImageLabeling() async {
-    results = "";
-
-    InputImage inputImage = InputImage.fromFile(image!);
-
-    final List<ImageLabel> labels = await labeler.processImage(inputImage);
-
-    for (ImageLabel label in labels) {
-      final String text = label.label;
-      final int index = label.index;
-      final double confidence = label.confidence;
-
-      debugPrint(text + "  " + confidence.toString());
-
-      results += text + "     " + confidence.toStringAsFixed(2) + "\n";
+  getModelPath(String asset) async {
+    final path = '${(await getApplicationSupportDirectory()).path}/$asset';
+    await Directory(dirname(path)).create(recursive: true);
+    final file = File(path);
+    if (!await file.exists()) {
+      final byteData = await rootBundle.load(asset);
+      await file.writeAsBytes(byteData.buffer
+          .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
     }
+    return file.path;
+  }
 
-    setState(() => results);
+  loadModel() async {
+    final modelPath = await getModelPath('assets/ml/model.tflite');
+    final options =
+        LocalLabelerOptions(confidenceThreshold: 0.8, modelPath: modelPath);
+    imageLabeler = ImageLabeler(options: options);
   }
 
   @override
@@ -92,24 +186,37 @@ class _HomePageScreenState extends State<HomePageScreen> {
         title: const Text('Fruit Detection'),
       ),
       body: Center(
-        child: SingleChildScrollView(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              image == null
-                  ? const Icon(Icons.image_outlined, size: 54)
-                  : Image.file(image!),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () => chooseImage(),
-                onLongPress: () => captureImage(),
-                child: const Text('Choose/Capture Image'),
+        children: [
+          if (controller.value.isInitialized)
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height / 1.5,
+                  child: AspectRatio(
+                      aspectRatio: controller.value.aspectRatio,
+                      child: CameraPreview(controller)),
+                ),
               ),
-              Text(results),
-            ],
-          ),
-        ),
-      ),
+            )
+          else
+            const CircularProgressIndicator(),
+          Card(
+              margin: const EdgeInsets.all(10),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    result,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ),
+              )),
+        ],
+      )),
     );
   }
 }
